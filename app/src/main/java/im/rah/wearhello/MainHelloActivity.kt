748c3c19c69037d18605d289ba8be2ef963e98d6
Rahim Packir Saibo <i.am@rah.im>
2019-01-03 23:01:36 +0000

    Naive Volley HTTP request on render onCreate for main Activity

--- app/src/main/java/im/rah/wearhello/MainHelloActivity.kt
+++ app/src/main/java/im/rah/wearhello/MainHelloActivity.kt
@@ -2,6 +2,11 @@ package im.rah.wearhello
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainHelloActivity : WearableActivity() {
 
@@ -11,5 +16,24 @@ class MainHelloActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+
+        val textView = findViewById<TextView>(R.id.text)
+        // ...
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://hugo-ns.herokuapp.com/api/v1/entries/current"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = response
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 }
