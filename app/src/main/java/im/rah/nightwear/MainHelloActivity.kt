c6f9883fd1b46ad54256a3b7f4ba73e72198d78d
Rahim Packir Saibo <i.am@rah.im>
2019-01-17 00:38:00 +0000

    App view: attempt BG refresh every 30 seconds

--- app/src/main/java/im/rah/nightwear/MainHelloActivity.kt
+++ app/src/main/java/im/rah/nightwear/MainHelloActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.widget.TextView
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainHelloActivity : WearableActivity() {
+
+    private lateinit var requestQueue : RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +22,21 @@ class MainHelloActivity : WearableActivity() {
         // Enables Always-on
         setAmbientEnabled()
 
+        requestQueue = Volley.newRequestQueue(this)
+
+        Timer().schedule(0, 1000 * 30) { runOnUiThread { refresh()} }
+    }
+
+    private fun refresh() {
         val textView = findViewById<TextView>(R.id.text)
-        // ...
-
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
         val url = "https://hugo-ns.herokuapp.com/api/v1/entries/current"
-
-        // Request a string response from the provided URL.
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response -> textView.text = BloodGlucose.parse_tab_separated_current(response).glucose() },
             Response.ErrorListener { textView.text = "That didn't work!" })
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+        requestQueue.add(stringRequest)
     }
-
-
+    
 }
