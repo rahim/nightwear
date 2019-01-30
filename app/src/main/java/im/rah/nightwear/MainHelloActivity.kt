cc2619822a4cb0b55cc40c5fa4e2c3214fa4a104
Rahim Packir Saibo <i.am@rah.im>
2019-01-30 22:53:09 +0000

    Refactor app version to use BloodGlucoseService

--- app/src/main/java/im/rah/nightwear/MainHelloActivity.kt
+++ app/src/main/java/im/rah/nightwear/MainHelloActivity.kt
@@ -3,17 +3,12 @@ package im.rah.nightwear
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.widget.TextView
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import java.util.*
 import kotlin.concurrent.schedule
 
 class MainHelloActivity : WearableActivity() {
 
-    private lateinit var requestQueue : RequestQueue
+    private lateinit var bloodGlucoseService: BloodGlucoseService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,26 +17,24 @@ class MainHelloActivity : WearableActivity() {
         // Enables Always-on
         setAmbientEnabled()
 
-        requestQueue = Volley.newRequestQueue(this)
+        bloodGlucoseService = BloodGlucoseService(this)
 
-        Timer().schedule(0, 1000 * 30) { runOnUiThread { refresh()} }
+        Timer().schedule(0, 1000) { runOnUiThread { refresh()} }
     }
 
     private fun refresh() {
         val textReading = findViewById<TextView>(R.id.textReading)
         val textReadingAge = findViewById<TextView>(R.id.textReadingAge)
-        val url = "https://hugo-ns.herokuapp.com/api/v1/entries/current"
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
-                val bg = BloodGlucose.parse_tab_separated_current(response)
-                textReading.text = bg.toString()
-                textReadingAge.text = bg.readingAge().toMinutes().toString() + "m"
-            },
-            Response.ErrorListener { textReading.text = "X" })
 
-        // Add the request to the RequestQueue.
-        requestQueue.add(stringRequest)
+        val bg:BloodGlucose? = bloodGlucoseService.latestBg
+        if (bg == null) {
+            textReading.text = "X"
+            textReadingAge.text = ""
+        }
+        else {
+            textReading.text = bg.toString()
+            textReadingAge.text = bg.readingAge().toMinutes().toString() + "m"
+        }
     }
 
 }
