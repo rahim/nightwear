b9ac1680f81559f796b772ab346b89b3a54c1c81
Rahim Packir Saibo <i.am@rah.im>
2019-02-20 23:56:15 +0000

    Add domain spinner

--- app/src/main/java/im/rah/nightwear/ConfigurationActivity.kt
+++ app/src/main/java/im/rah/nightwear/ConfigurationActivity.kt
@@ -2,8 +2,16 @@ package im.rah.nightwear
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.AdapterView
+
+
 
 class ConfigurationActivity : WearableActivity() {
 
@@ -11,17 +19,57 @@ class ConfigurationActivity : WearableActivity() {
         val TLDS = arrayOf("herokuapp.com", "azurewebsites.net")
     }
 
+    val scheme = "https://"
+
+    lateinit var urlTextView:TextView
+    lateinit var domainEditText:EditText
+    lateinit var tldSpinner:Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
 
-        val tldTextView = findViewById<AutoCompleteTextView>(R.id.tld)
-        tldTextView.threshold = 1
-        ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, TLDS).also { adapter ->
-            tldTextView.setAdapter(adapter)
+        tldSpinner = findViewById(R.id.tld)
+        ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, TLDS).also { adapter ->
+            tldSpinner.adapter = adapter
         }
+
+        domainEditText = findViewById(R.id.domain)
+        urlTextView = findViewById(R.id.url)
+
+        tldSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View, position: Int, id: Long) {
+                refreshUrlText()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {
+                refreshUrlText()
+            }
+        }
+        domainEditText.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                refreshUrlText()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+        })
+        refreshUrlText()
 
         // Enables Always-on
         setAmbientEnabled()
+    }
+
+    fun refreshUrlText() {
+        urlTextView.text = url()
+    }
+
+    fun url() : String {
+        return scheme + domainEditText.text + "." + tldSpinner.selectedItem
     }
 }
