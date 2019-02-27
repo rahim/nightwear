d499a6e7cd3afc7adb487d8eaa382e2b64da79fa
Rahim Packir Saibo <i.am@rah.im>
2019-02-27 22:23:29 +0000

    Add configuration activity for setting nightscout URL. (#4)
    * Configuration activity scaffold
    
    * Flesh configuration activity, failed attempt at using AutoCompleteTextView
    
    * Add domain spinner
    
    * Load and save nightscout url to SharedPreferences
    
    * Minimal viable nightscout URL configuration
    
    - Functional implementation of BloodGlucoseService observing and using configured nightscout base URL
    - Problem: requests are made against partial domains during editing
    - Problem: requests are made against default domain
    - Problem: poor feedback on misstyped/invalid URLs
    
    * Handle invalid responses on attempted API requests
    
    * Improve SharedPreference handling
    
    * Add save domain button
    
    * Bump gradle version

--- app/src/main/java/im/rah/nightwear/BloodGlucoseService.kt
+++ app/src/main/java/im/rah/nightwear/BloodGlucoseService.kt
@@ -1,30 +1,41 @@
 package im.rah.nightwear
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import java.text.ParseException
 import java.time.Duration
 import java.time.Instant
 import java.util.*
 import kotlin.concurrent.schedule
 
-class BloodGlucoseService(context: Context) {
-    var nightscoutUrl = "https://hugo-ns.herokuapp.com/api/v1/entries/current"
-    private val requestQueue: RequestQueue = Volley.newRequestQueue(context)
+class BloodGlucoseService(context: Context) : SharedPreferences.OnSharedPreferenceChangeListener {
     var latestBg:BloodGlucose? = null
-    var lastRequestAdded:Instant = Instant.EPOCH
+
+    private var nightscoutBaseUrl = ""
+    private val requestQueue: RequestQueue = Volley.newRequestQueue(context)
+    lateinit private var prefs: SharedPreferences
+    private var lastRequestAdded:Instant = Instant.EPOCH
 
     companion object {
         const val TAG:String = "BloodGlucoseService"
         val SENSOR_REFRESH_INTERVAL:Duration = Duration.ofMinutes(5)
+
+        const val NS_CURRENT_ENTRY_PATH = "/api/v1/entries/current"
     }
 
     init {
         Log.d(TAG, "initing")
+        prefs = PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
+        prefs.registerOnSharedPreferenceChangeListener(this)
+        nightscoutBaseUrl = prefs.getString("nightscoutBaseUrl", "")
+
         Timer().schedule(0, 1000 * 15) { refresh() }
     }
 
@@ -40,20 +51,36 @@ class BloodGlucoseService(context: Context) {
         }
     }
 
+    override fun onSharedPreferenceChanged(prefs: SharedPreferences, s: String) {
+        Log.d(TAG, "prefs changed")
+        nightscoutBaseUrl = prefs.getString("nightscoutBaseUrl", "")
+    }
+
+    private fun nsCurrentEntryUrl() : String {
+        return nightscoutBaseUrl + NS_CURRENT_ENTRY_PATH
+    }
+
     private fun refresh() {
         Log.d(TAG, "refresh")
         Log.d(TAG, "Latest reading age: " + latestReadingAge().seconds)
+        Log.d(TAG, "nightscoutBaseUrl: " + nightscoutBaseUrl)
 
+        if (nightscoutBaseUrl == "") return
         if (latestReadingAge() < SENSOR_REFRESH_INTERVAL) return
         if (Duration.between(lastRequestAdded, Instant.now()) < Duration.ofSeconds(30)) return
 
         Log.d(TAG, "requesting")
         lastRequestAdded = Instant.now()
         val stringRequest = StringRequest(
-            Request.Method.GET, nightscoutUrl,
+            Request.Method.GET, nsCurrentEntryUrl(),
             Response.Listener<String> { response ->
                 Log.d(TAG, "bg received, parsing...")
-                latestBg = BloodGlucose.parse_tab_separated_current(response)
+                try {
+                    latestBg = BloodGlucose.parseTabSeparatedCurrent(response)
+                }
+                catch (e: ParseException) {
+                    Log.d(TAG, "ParseException for response: " + response)
+                }
             },
             Response.ErrorListener {
                 Log.d(TAG, "request error")
---
+++ app/src/main/java/im/rah/nightwear/ConfigurationActivity.kt
@@ -0,0 +1,126 @@
+package im.rah.nightwear
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.wearable.activity.WearableActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.*
+
+class ConfigurationActivity : WearableActivity() {
+
+    companion object {
+        const val TAG:String = "ConfigurationActivity"
+        private val TLDS = arrayOf("herokuapp.com", "azurewebsites.net")
+        private const val DEFAULT_URL = "https://domain.herokuapp.com"
+    }
+
+    private val scheme = "https://"
+
+    private lateinit var urlTextView:TextView
+    private lateinit var domainEditText:EditText
+    private lateinit var tldSpinner:Spinner
+    private lateinit var saveButton:Button
+
+    private lateinit var prefs:SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this.applicationContext)
+
+        setContentView(R.layout.activity_configuration)
+
+        tldSpinner = findViewById(R.id.tld)
+        ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, TLDS).also { adapter ->
+            tldSpinner.adapter = adapter
+        }
+
+        domainEditText = findViewById(R.id.domain)
+        urlTextView = findViewById(R.id.url)
+
+        tldSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View, position: Int, id: Long) {
+                refreshUrlText()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {
+                refreshUrlText()
+            }
+        }
+        domainEditText.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d(TAG, "onTextChanged")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d(TAG, "afterTextChanged")
+                refreshUrlText()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d(TAG, "beforeTextChanged")
+            }
+        })
+
+        saveButton = findViewById(R.id.save_button)
+        saveButton.setOnClickListener { persistUrl() }
+
+        loadUrlFromPrefs()
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+
+    private fun loadUrlFromPrefs() {
+        val url = prefs.getString("nightscoutBaseUrl", DEFAULT_URL)
+        val domain = domainFromUrl(url)
+        domainEditText.setText(domain)
+
+        val tld = tldFromUrl(url)
+        val adapter = tldSpinner.adapter as ArrayAdapter<String>
+        val tldPosition = adapter.getPosition(tld)
+        tldSpinner.setSelection(tldPosition)
+    }
+
+    private fun domainFromUrl(url:String) : String {
+        val tld = tldFromUrl(url)
+        val tldStrippedUrl = if (tld.isNotBlank())
+                               url.replace(tld, "")
+                             else
+                               url
+        return tldStrippedUrl.replace(scheme, "").removeSuffix(".")
+    }
+
+    private fun tldFromUrl(url:String) : String {
+        for (tld in TLDS) if (url.endsWith(tld)) return tld
+        return ""
+    }
+
+    private fun refreshUrlText() {
+        Log.d(TAG, "refreshUrlText")
+        val url = url()
+        if (urlTextView.text != url) {
+            Log.d(TAG, "updating urlTextView")
+            urlTextView.text = url
+        }
+    }
+
+    private fun persistUrl() {
+        Log.d(TAG, "persistUrl")
+        val url = url()
+        if (prefs.getString("nighscoutBaseUrl", "") != url) {
+            Log.d(TAG, "updating nighscoutBaseUrl pref")
+            val edit = prefs.edit()
+            edit.putString("nightscoutBaseUrl", url())
+            edit.apply()
+        }
+    }
+
+    private fun url() : String {
+        return scheme + domainEditText.text + "." + tldSpinner.selectedItem
+    }
+}
