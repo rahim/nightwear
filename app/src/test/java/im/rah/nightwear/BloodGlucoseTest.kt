d7b6cc78ff39e3115281c463030ac40e573292fc
Rahim Packir Saibo <i.am@rah.im>
2020-10-28 09:55:24 +0000

    Add minimal BloodGlucose.parseTabSeparatedCurrent tests

--- app/src/test/java/im/rah/nightwear/BloodGlucoseTest.kt
+++ app/src/test/java/im/rah/nightwear/BloodGlucoseTest.kt
@@ -5,7 +5,25 @@ import org.junit.Test
 
 class BloodGlucoseTest {
     @Test
-    fun assertTruth() {
-        assertThat(true).isEqualTo(true)
+    fun testParseTabSeparatedCurrent() {
+        val exampleResponse = "2019-01-07T21:20:50.000Z	1546896050000\t109\tFlat\tshare2"
+
+        val bloodGlucose = BloodGlucose.parseTabSeparatedCurrent(exampleResponse)
+
+        assertThat(bloodGlucose?.glucoseLevel_mgdl).isEqualTo(109)
+        assertThat(bloodGlucose?.direction).isEqualTo(BloodGlucose.Direction.Flat)
+        assertThat(bloodGlucose?.sensorTime).isEqualTo(1546896050000)
     }
+
+    @Test
+    fun testParseTabSeparatedCurrentWithQuoteWrapping() {
+        val exampleResponse = "2019-01-07T21:20:50.000Z	1546896050000\t109\t\"Flat\"\t\"share2\""
+
+        val bloodGlucose = BloodGlucose.parseTabSeparatedCurrent(exampleResponse)
+
+        assertThat(bloodGlucose?.glucoseLevel_mgdl).isEqualTo(109)
+        assertThat(bloodGlucose?.direction).isEqualTo(BloodGlucose.Direction.Flat)
+        assertThat(bloodGlucose?.sensorTime).isEqualTo(1546896050000)
+    }
+
 }
