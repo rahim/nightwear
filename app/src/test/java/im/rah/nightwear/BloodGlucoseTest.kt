2dbbe29c6b901a4f17df2df327ddcc34b85ca1d1
Rahim Packir Saibo <i.am@rah.im>
2020-10-28 00:26:52 +0000

    Add test harness and useless example test

---
+++ app/src/test/java/im/rah/nightwear/BloodGlucoseTest.kt
@@ -0,0 +1,11 @@
+package im.rah.nightwear
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class BloodGlucoseTest {
+    @Test
+    fun assertTruth() {
+        assertThat(true).isEqualTo(true)
+    }
+}
