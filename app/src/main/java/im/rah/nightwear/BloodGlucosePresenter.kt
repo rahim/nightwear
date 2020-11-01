353f63763712583684310e8f6fd1bfd4bc22022d
Rahim Packir Saibo <i.am@rah.im>
2020-11-01 22:52:39 +0000

    Make currently unused members private

--- app/src/main/java/im/rah/nightwear/BloodGlucosePresenter.kt
+++ app/src/main/java/im/rah/nightwear/BloodGlucosePresenter.kt
@@ -1,10 +1,10 @@
 package im.rah.nightwear
 
 class BloodGlucosePresenter(private val bg: BloodGlucose, private val mmol: Boolean = true) {
-    fun glucose() = BloodGlucose.glucose(bg.glucoseLevel_mgdl, mmol)
-    fun directionLabel(saferUnicode: Boolean = false) =
+    private fun glucose() = BloodGlucose.glucose(bg.glucoseLevel_mgdl, mmol)
+    private fun directionLabel(saferUnicode: Boolean = false) =
         if (saferUnicode) bg.direction.saferLabel else bg.direction.bolderLabel
-    fun annotation(markOld: Boolean, saferUnicode: Boolean = false) : String {
+    private fun annotation(markOld: Boolean, saferUnicode: Boolean = false) : String {
         return when {
             markOld && bg.readingAge() > BloodGlucose.OLD_READING_THRESHOLD -> "OLD"
             else -> directionLabel(saferUnicode)
