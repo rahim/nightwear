daa2c6923fef498f514b16ae82cee882da04eee6
Rahim Packir Saibo <i.am@rah.im>
2022-04-25 23:25:26 +0100

    Commenting: explain the conversion rounding problem

--- app/src/main/java/im/rah/nightwear/BloodGlucoseDelta.kt
+++ app/src/main/java/im/rah/nightwear/BloodGlucoseDelta.kt
@@ -1,5 +1,10 @@
 package im.rah.nightwear
 
+// The modelling here is clean and intuitive, but using mg/dL differences will lead to rounding
+// inconsistencies for mmol/L with both the NightScout displayed deltas, but also with what we
+// we expect from the change observed.
+//
+// To overcome this we'll need to convert units before calculating the difference.
 class BloodGlucoseDelta(val delta_mgdl: Int) {
     companion object {
         fun between(first: BloodGlucose, second: BloodGlucose): BloodGlucoseDelta {
